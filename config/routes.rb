185fa842605358ab9d2c8243db09bde369cd9e06
nadesen <mononokehimehafukai@gmail.com>
2025-10-15 08:41:07 +0000

    public/homesへ変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "homes#top"
-  get "homes/about", to: "home#about"
+  root to: "public/homes#top"
+  get "about", to: "public/homes#about", as: "homes_about"
 end
