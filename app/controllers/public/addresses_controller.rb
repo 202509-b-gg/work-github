56af3155bd748c685efb4aca07262c108a9b33b2
Ubuntu <ubuntu@ip-172-31-16-185.ap-northeast-1.compute.internal>
2025-10-17 06:03:28 +0000

    配送先関連機能実装

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -5,30 +5,44 @@ class Public::AddressesController < ApplicationController
   def index
     @customer = current_customer
     @addresses = @customer.addresses
+    @address = Address.new
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
     @address = Address.new(address_params)
     @address.customer_id = current_customer.id
     if @address.save
-      redirect_to address_path
+      redirect_to addresses_path
     else
       render 'index'
     end
   end
 
   def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @address = Address.find(params[:id])
+    if @address.destroy
+      redirect_to addresses_path
+    else
+      render 'index'
+    end
   end
 
   private
 
   def address_params
-    params.permit(:postal_code, :address, :recipient_name)
+    params.require(:address).permit(:postal_code, :address, :recipient_name)
   end
 end
