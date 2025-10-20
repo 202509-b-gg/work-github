ed9a70c5ec4ad72e1f4618bf2344df915cde4ff2
Ubuntu <ubuntu@ip-172-31-26-254.ap-northeast-1.compute.internal>
2025-10-20 08:58:03 +0000

    管理者画面　new edit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,6 +4,18 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+    @genres = Genre.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      @genres = Genre.all
+      render :new
+    end
   end
 
   def show
@@ -11,5 +23,22 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render "edit"
+    end
+  end
+
+private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
   end
 end
