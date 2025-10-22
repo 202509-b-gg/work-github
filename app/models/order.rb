c5022b9020928f0d67ff2b14ffc5c82dd941cf04
nadesen <mononokehimehafukai@gmail.com>
2025-10-22 08:07:03 +0000

    カート、オーダーの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,15 @@ class Order < ApplicationRecord
 
   enum status: { waiting_for_payment: 0, payment_confirmed: 1, in_production: 2, preparing_for_shipping: 3, shipped: 4 }
 
+  validates :customer_id, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :shipping_cost, presence: true
+  validates :total_payment, presence: true
+  validates :status, presence: true
+  validates :payment_method, presence: true
+
   def status_i18n
     I18n.t("enums.order.status.#{self.status}")
   end
